fa4e51b70576427b38de20b78e3b4f775f123f51
Jake Mackinlay <jrmackinlay90@gmail.com>
2020-10-17 19:12:09 +1030

    added questions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,66 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title for this repo?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "A brief description of the application?"
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "How do you install this application?"
+      },
+      {
+        type: "input",
+        name: "contributions",
+        message: "Are there any contribution guidelines to follow?"
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "Are there any instructions regarding testing"
+      },
+      {
+        type: "list",
+        message: "What licensing is needed for this application",
+        name: "license",
+        choices: [
+          "Apache license 2.0",
+          "MIT",
+          "GNU General Public License v3.0",
+          "Academic Free License v3.0"
+        ]
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter your github user name"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your email address"
+      },
 ];
 
+
+
 // function to write README file
-function writeToFile(fileName, data) {
+async function writeToFile(fileName, data) {
+
+    inquirer.prompt(questions);
+    
+    fs.writeFile(data.title + '.txt', data, (err) => {
+        if (err) throw err;
+    })
 }
 
 // function to initialize program
@@ -14,3 +70,6 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+writeToFile()
